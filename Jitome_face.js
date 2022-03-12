d405c51b5630b10b1d86d0de9946f77b245aa7f2
NorthTangent <101153534+NorthTangent@users.noreply.github.com>
2022-03-12 17:43:32 +0900

    アップデートなど

---
+++ Jitome_face.js
@@ -0,0 +1,6 @@
+const Jito_Face = {
+    'Normal': 'images/Jitome/01_normal.png',
+    'Good': 'images/Jitome/02_kira.png',
+    'Dark': 'images/Jitome/03_kuro.png',
+    'Thinking': 'images/Jitome/04_hmm.png'
+}
--- omikuji.js
+++ omikuji.js
@@ -3,12 +3,14 @@ const App = ({
         return{
             menu: 1,
             howmany: 0,
+            isKotei: 0,
             today_lucky: "大吉"
         }
     },
     methods: {
         start(){
-            this.Jitome_Face = "./images/Jitome/01_normal.png"
+            this.Jitome_Face = Jito_Face['Normal'];
+            this.isKotei = 1;
             this.menu = 2;
         }
     }
